366f6d135931f1f6b9defbca8bb245912a76a29f
kyle powers <wefilm@yahoo.com>
2016-02-11 16:32:23 -0500

    new fina

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -1,56 +1,48 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
+using System.Collections;
 
 public class Basket : MonoBehaviour {
-
-	public GUIText scoreGT; // 1
-
-	private int count
-
-	void Update () 
-	{
+	public Text scoreGT,scoreGO;                                         
+	// Use this for initialization
+	void Start () {
+		// Find a reference to the ScoreCounter GameObject
+		scoreGO =  GameObject.Find("ScoreCounter").GetComponent<Text>();
+		// Get the GUIText Component of that GameObject
+		scoreGT = scoreGO.GetComponent<Text>();                         
+		scoreGT.text = "0";
+	}
+	// Update is called once per frame
+	void Update () {
 		// Get the current screen position of the mouse from Input
-		Vector3 mousePos2D = Input.mousePosition; // 1
+		Vector3 mousePos2D = Input.mousePosition;                          
 
 		// The Camera's z position sets the how far to push the mouse into 3D
-		mousePos2D.z = -Camera.main.transform.position.z; // 2
+		mousePos2D.z = -Camera.main.transform.position.z;                   
 
 		// Convert the point from 2D screen space into 3D game world space
-		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D ); // 3
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D );  
 
 		// Move the x position of this Basket to the x position of the Mouse
 		Vector3 pos = this.transform.position;
 		pos.x = mousePos3D.x;
 		this.transform.position = pos;
 	}
-	void Start() 
-	{
-		// Find a reference to the ScoreCounter GameObject
-		GameObject scoreGO = GameObject.Find("ScoreCounter"); // 2
-		// Get the GUIText Component of that GameObject
-		scoreGT = scoreGO.GetComponent<GUIText>(); // 3
-		// Set the starting number of points to 0
-		scoreGT.text = "0";
-	}
-
-	void OnCollisionEnter( Collision coll ) 
-	{ // 2
+	void OnCollisionEnter( Collision coll ) {                               
 		// Find out what hit this basket
-		GameObject collidedWith = coll.gameObject; // 3
-		if (collidedWith.tag == "Apple") { // 4
-			Destroy (collidedWith);
+		GameObject collidedWith = coll.gameObject;                          
+		if ( collidedWith.tag == "Apple" ) {                                
+			Destroy( collidedWith );
 		}
 		// Parse the text of the scoreGT into an int
-		int score = int.Parse (scoreGT.text); // 4
+		int score = int.Parse(scoreGT.text);                             
 		// Add points for catching the apple
 		score += 100;
 		// Convert the score back to a string and display it
-		scoreGT.text = score.ToString ();
+		scoreGT.text = score.ToString();
 		// Track the high score
 		if (score > HighScore.score) {
 			HighScore.score = score;
 		}
-			
 	}
 }
